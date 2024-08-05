1f4454132af03ed2ca9867e3ec6f01cea2e2da28
Tatyana D. <93326105+taty-anna@users.noreply.github.com>
2024-08-05 16:45:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,20 +2,17 @@
 This is the main file where the analysis comes together.
 
 """
-"""
-This is the main file where the analysis comes together.
-
-"""
 # Insert functions from other .py files
 from data_preprocessing import preprocess_data
 from revenues import calculate_basic_strategy_revenues
 from optimised_model import optimize_battery
 from vis_tool_dash import app
+from visualisation import plot_revenues
 
 
 if __name__ == "__main__":
-    file_name = 'input_data.csv' # Open original price data
-    prices_data = preprocess_data(file_name) # Clean the price data using pre-developed function
+    file_name = 'input_data.csv'  # Open original price data
+    prices_data = preprocess_data(file_name)  # Clean the price data using pre-developed function
 
     # Calculate revenues for the baseline strategy
     daily_revenue_df, annual_revenue_df = calculate_basic_strategy_revenues(prices_data, capacity=100, power=100,
@@ -38,6 +35,9 @@ if __name__ == "__main__":
     # Save CSV file with annual revenues from the optimised model, /
     # file name will adjust accordingly to the cycles chosen
     annual_opt_revenues.to_csv(f'annual_opt_results_cycles_{cycles}.csv', index=False)
+
+    # Plot revenues comparing baseline and optimised strategies
+    plot_revenues(annual_revenue_df, annual_opt_revenues)
 
     # Run the Dash Plotly visualisation tool - Run in browser 'http://127.0.0.1:8050'
     app.run_server(host='127.0.0.1', port=8050, debug=True)
